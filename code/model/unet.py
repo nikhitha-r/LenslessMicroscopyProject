f2f7514562bcaed66606291eab31e0057c171729
Maha <maha.sabanayagam@tum.de>
2019-05-19 11:52:43 +0200

    UNet model for segmentation

---
+++ code/model/unet.py
@@ -0,0 +1,277 @@
+import os
+import shutil
+import numpy as np
+from collections import OrderedDict
+import logging
+
+import tensorflow as tf
+
+from tf_unet import util
+from tf_unet.layers import (weight_variable, weight_variable_devonc, bias_variable,
+                            conv2d, deconv2d, max_pool, crop_and_concat, pixel_wise_softmax,
+                            cross_entropy)
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+
+def create_conv_net(x, keep_prob, channels, n_class, layers=3, features_root=16, filter_size=3, pool_size=2,
+                    summaries=True):
+    """
+    Creates a new convolutional unet for the given parametrization.
+    :param x: input tensor, shape [?,nx,ny,channels]
+    :param keep_prob: dropout probability tensor
+    :param channels: number of channels in the input image
+    :param n_class: number of output labels
+    :param layers: number of layers in the net
+    :param features_root: number of features in the first layer
+    :param filter_size: size of the convolution filter
+    :param pool_size: size of the max pooling operation
+    :param summaries: Flag if summaries should be created
+    """
+
+    logging.info(
+        "Layers {layers}, features {features}, filter size {filter_size}x{filter_size}, pool size: {pool_size}x{pool_size}".format(
+            layers=layers,
+            features=features_root,
+            filter_size=filter_size,
+            pool_size=pool_size))
+
+    # Placeholder for the input image
+    with tf.name_scope("preprocessing"):
+        nx = tf.shape(x)[1]
+        ny = tf.shape(x)[2]
+        x_image = tf.reshape(x, tf.stack([-1, nx, ny, channels]))
+        in_node = x_image
+        batch_size = tf.shape(x_image)[0]
+
+    weights = []
+    biases = []
+    convs = []
+    pools = OrderedDict()
+    deconv = OrderedDict()
+    dw_h_convs = OrderedDict()
+    up_h_convs = OrderedDict()
+
+    in_size = 1000
+    size = in_size
+    # down layers
+    for layer in range(0, layers):
+        with tf.name_scope("down_conv_{}".format(str(layer))):
+            features = 2 ** layer * features_root
+            stddev = np.sqrt(2 / (filter_size ** 2 * features))
+            if layer == 0:
+                w1 = weight_variable([filter_size, filter_size, channels, features], stddev, name="w1")
+            else:
+                w1 = weight_variable([filter_size, filter_size, features // 2, features], stddev, name="w1")
+
+            w2 = weight_variable([filter_size, filter_size, features, features], stddev, name="w2")
+            b1 = bias_variable([features], name="b1")
+            b2 = bias_variable([features], name="b2")
+
+            conv1 = conv2d(in_node, w1, b1, keep_prob)
+            tmp_h_conv = tf.nn.relu(conv1)
+            conv2 = conv2d(tmp_h_conv, w2, b2, keep_prob)
+            dw_h_convs[layer] = tf.nn.relu(conv2)
+
+            weights.append((w1, w2))
+            biases.append((b1, b2))
+            convs.append((conv1, conv2))
+
+            size -= 2 * 2 * (filter_size // 2) # valid conv
+            if layer < layers - 1:
+                pools[layer] = max_pool(dw_h_convs[layer], pool_size)
+                in_node = pools[layer]
+                size /= pool_size
+
+    in_node = dw_h_convs[layers - 1]
+
+    # up layers
+    for layer in range(layers - 2, -1, -1):
+        with tf.name_scope("up_conv_{}".format(str(layer))):
+            features = 2 ** (layer + 1) * features_root
+            stddev = np.sqrt(2 / (filter_size ** 2 * features))
+
+            wd = weight_variable_devonc([pool_size, pool_size, features // 2, features], stddev, name="wd")
+            bd = bias_variable([features // 2], name="bd")
+            h_deconv = tf.nn.relu(deconv2d(in_node, wd, pool_size) + bd)
+            h_deconv_concat = crop_and_concat(dw_h_convs[layer], h_deconv)
+            deconv[layer] = h_deconv_concat
+
+            w1 = weight_variable([filter_size, filter_size, features, features // 2], stddev, name="w1")
+            w2 = weight_variable([filter_size, filter_size, features // 2, features // 2], stddev, name="w2")
+            b1 = bias_variable([features // 2], name="b1")
+            b2 = bias_variable([features // 2], name="b2")
+
+            conv1 = conv2d(h_deconv_concat, w1, b1, keep_prob)
+            h_conv = tf.nn.relu(conv1)
+            conv2 = conv2d(h_conv, w2, b2, keep_prob)
+            in_node = tf.nn.relu(conv2)
+            up_h_convs[layer] = in_node
+
+            weights.append((w1, w2))
+            biases.append((b1, b2))
+            convs.append((conv1, conv2))
+
+            size *= pool_size
+            size -= 2 * 2 * (filter_size // 2) # valid conv
+
+    # Output Map
+    with tf.name_scope("output_map"):
+        weight = weight_variable([1, 1, features_root, n_class], stddev)
+        bias = bias_variable([n_class], name="bias")
+        conv = conv2d(in_node, weight, bias, tf.constant(1.0))
+        output_map = tf.nn.relu(conv)
+        up_h_convs["out"] = output_map
+
+    if summaries:
+        with tf.name_scope("summaries"):
+            for i, (c1, c2) in enumerate(convs):
+                tf.summary.image('summary_conv_%02d_01' % i, get_image_summary(c1))
+                tf.summary.image('summary_conv_%02d_02' % i, get_image_summary(c2))
+
+            for k in pools.keys():
+                tf.summary.image('summary_pool_%02d' % k, get_image_summary(pools[k]))
+
+            for k in deconv.keys():
+                tf.summary.image('summary_deconv_concat_%02d' % k, get_image_summary(deconv[k]))
+
+            for k in dw_h_convs.keys():
+                tf.summary.histogram("dw_convolution_%02d" % k + '/activations', dw_h_convs[k])
+
+            for k in up_h_convs.keys():
+                tf.summary.histogram("up_convolution_%s" % k + '/activations', up_h_convs[k])
+
+    variables = []
+    for w1, w2 in weights:
+        variables.append(w1)
+        variables.append(w2)
+
+    for b1, b2 in biases:
+        variables.append(b1)
+        variables.append(b2)
+
+    return output_map, variables, int(in_size - size)
+
+
+class Unet(object):
+    """
+    A unet implementation
+    :param channels: number of channels in the input image
+    :param n_class: number of output labels
+    :param cost: (optional) name of the cost function. Default is 'cross_entropy'
+    :param cost_kwargs: (optional) kwargs passed to the cost function. See Unet._get_cost for more options
+    """
+
+    def __init__(self, channels, n_class, cost="cross_entropy", cost_kwargs={}, **kwargs):
+        tf.reset_default_graph()
+
+        self.n_class = n_class
+        self.summaries = kwargs.get("summaries", True)
+
+        self.x = tf.placeholder("float", shape=[None, None, None, channels], name="x")
+        self.y = tf.placeholder("float", shape=[None, None, None, n_class], name="y")
+        self.keep_prob = tf.placeholder(tf.float32, name="dropout_probability")  # dropout (keep probability)
+
+        logits, self.variables, self.offset = create_conv_net(self.x, self.keep_prob, channels, n_class, **kwargs)
+
+        self.cost = self._get_cost(logits, cost, cost_kwargs)
+
+        self.gradients_node = tf.gradients(self.cost, self.variables)
+
+        with tf.name_scope("cross_entropy"):
+            self.cross_entropy = cross_entropy(tf.reshape(self.y, [-1, n_class]),
+                                               tf.reshape(pixel_wise_softmax(logits), [-1, n_class]))
+
+        with tf.name_scope("results"):
+            self.predicter = pixel_wise_softmax(logits)
+            self.correct_pred = tf.equal(tf.argmax(self.predicter, 3), tf.argmax(self.y, 3))
+            self.accuracy = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32))
+
+    def _get_cost(self, logits, cost_name, cost_kwargs):
+        """
+        Constructs the cost function, either cross_entropy, weighted cross_entropy or dice_coefficient.
+        Optional arguments are:
+        class_weights: weights for the different classes in case of multi-class imbalance
+        regularizer: power of the L2 regularizers added to the loss function
+        """
+
+        with tf.name_scope("cost"):
+            flat_logits = tf.reshape(logits, [-1, self.n_class])
+            flat_labels = tf.reshape(self.y, [-1, self.n_class])
+            if cost_name == "cross_entropy":
+                class_weights = cost_kwargs.pop("class_weights", None)
+
+                if class_weights is not None:
+                    class_weights = tf.constant(np.array(class_weights, dtype=np.float32))
+
+                    weight_map = tf.multiply(flat_labels, class_weights)
+                    weight_map = tf.reduce_sum(weight_map, axis=1)
+
+                    loss_map = tf.nn.softmax_cross_entropy_with_logits_v2(logits=flat_logits,
+                                                                          labels=flat_labels)
+                    weighted_loss = tf.multiply(loss_map, weight_map)
+
+                    loss = tf.reduce_mean(weighted_loss)
+
+                else:
+                    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=flat_logits,
+                                                                                     labels=flat_labels))
+            elif cost_name == "dice_coefficient":
+                eps = 1e-5
+                prediction = pixel_wise_softmax(logits)
+                intersection = tf.reduce_sum(prediction * self.y)
+                union = eps + tf.reduce_sum(prediction) + tf.reduce_sum(self.y)
+                loss = -(2 * intersection / (union))
+
+            else:
+                raise ValueError("Unknown cost function: " % cost_name)
+
+            regularizer = cost_kwargs.pop("regularizer", None)
+            if regularizer is not None:
+                regularizers = sum([tf.nn.l2_loss(variable) for variable in self.variables])
+                loss += (regularizer * regularizers)
+
+            return loss
+
+    def predict(self, model_path, x_test):
+        """
+        Uses the model to create a prediction for the given data
+        :param model_path: path to the model checkpoint to restore
+        :param x_test: Data to predict on. Shape [n, nx, ny, channels]
+        :returns prediction: The unet prediction Shape [n, px, py, labels] (px=nx-self.offset/2)
+        """
+
+        init = tf.global_variables_initializer()
+        with tf.Session() as sess:
+            # Initialize variables
+            sess.run(init)
+
+            # Restore model weights from previously saved model
+            self.restore(sess, model_path)
+
+            y_dummy = np.empty((x_test.shape[0], x_test.shape[1], x_test.shape[2], self.n_class))
+            prediction = sess.run(self.predicter, feed_dict={self.x: x_test, self.y: y_dummy, self.keep_prob: 1.})
+
+        return prediction
+
+    def save(self, sess, model_path):
+        """
+        Saves the current session to a checkpoint
+        :param sess: current session
+        :param model_path: path to file system location
+        """
+
+        saver = tf.train.Saver()
+        save_path = saver.save(sess, model_path)
+        return save_path
+
+    def restore(self, sess, model_path):
+        """
+        Restores a session from a checkpoint
+        :param sess: current session instance
+        :param model_path: path to file system checkpoint location
+        """
+
+        saver = tf.train.Saver()
+        saver.restore(sess, model_path)
+        logging.info("Model restored from file: %s" % model_path)
