2e5054936d5f79a9f2360974c1a6c9b9483d4f82
Maha <maha.sabanayagam@tum.de>
2019-05-25 11:17:34 +0200

    Generating annotations with different filtering techniques. Use argument 0,1,2 for gaussian, median(5*5), median(3*3) filters

--- code/matlab/exampleSegmentation.m
+++ code/matlab/exampleSegmentation.m
@@ -1,33 +1,69 @@
-
-img = imread('helamicroscopy.png');
-
-
-%let's resize it a bit to speed it up
-img_re = imresize(img,0.5);
-img_s = img_re;
-%img_s = img_re(1:1039,1:1387);
 %%
-sigma = 2;%1.5;
-img_sfilt = imgaussfilt(img_s,sigma);
-
-figure;
-imshow(img_sfilt)
-
+%arg1 = 0 for gaussian filter with sigma 2
+%       1 for median filter with 5*5 window size
+%       2 for median filter with 3*3 window size (default)
 %%
+%% TO NOTE: Not cropping the image boundaries! Do it when processing it later.'visualize',true);
+function [] = exapmleSegmentation(arg1)
+    if nargin == 0 
+        arg1 = '2' %default 
+    end
+    display(arg1)
+    direc = dir('/home/aravind/Desktop/Maha/IDP/DI_Lab/code/data/test_data_00_39/');
+    save_direc = '/home/aravind/Desktop/Maha/IDP/DI_Lab/code/data/test_data_mask/';
+   
+    for file = direc'
+        if endsWith(file.name, '.png')
+            fname = strcat(file.folder ,'/', file.name);
+            img = imread(fname);
 
-tiledim = 30;
+            %let's resize it a bit to speed it up
+            img_re = imresize(img,0.5);
+            img_s = img_re;
 
-lambda = 5; minSizeMSER = 30; maxSizeMSER = 4000; maxVariation = 1;
+            %img_s = img_re(1:1039,1:1387);
+            
+            if arg1 == '0' 
+                sigma = 2; %1.5;
+                img_sfilt = imgaussfilt(img_s,sigma);
+            elseif arg1 == '1'
+                 img_med_filter = medfilt2(img_s, [5,5]);
+                 %img_sfilt = histeq(img_med_filter)
+                 %img_sfilt = adapthisteq(img_med_filter,'NumTiles',[12 12],'ClipLimit',0.003);
+                img_sfilt = img_med_filter;
+            elseif arg1 == '2' 
+                img_med_filter = medfilt2(img_s, [3,3]);
+                img_sfilt = img_med_filter;
+            else
+                error("No implementation found!");
+            end
 
-maxEcc = .7; minSizeSplit = 30; maxSizeSplit = 1000;
+            %figure;
+            %imshow(img_sfilt)
 
+            tiledim = 30;
 
-%run the code (don't be scared it takes a bit)
-bw = segmentImage(img_sfilt,'visualize',true);
+            lambda = 5; minSizeMSER = 30; maxSizeMSER = 4000; maxVariation = 1;
 
-%% rescale it back
-bw_back = imresize(bw,2);
-%dilate a bit the foreground such that closely neighboured cells appear in
-%one cluster
-figure;
-imagesc(bw_back);colormap gray;
+            maxEcc = .7; minSizeSplit = 30; maxSizeSplit = 1000;
+
+            imgname = strsplit(file.name, '.');
+            %run the code (don't be scared it takes a bit)
+            bw = segmentImage(img_sfilt,'visualize',false);
+
+            %% rescale it back
+            bw_back = imresize(bw,2);
+            %dilate a bit the foreground such that closely neighboured cells appear in
+            %one cluster
+
+            %se = strel('sphere',1); Tried dilating. Can use it while
+            %checking with the lensfree images. Shows good result.
+            %bw_back = imdilate(bw_back,se);
+            fname = strcat(save_direc , imgname{1} , '.png');
+            imwrite(bw_back, fname);
+            
+            %figure;
+            %imagesc(bw_back);colormap gray;
+        end
+    end
+end
