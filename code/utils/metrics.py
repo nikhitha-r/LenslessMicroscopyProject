aea84c84f91a9578941ef9a8c4169f4c8406ba95
Nikhitha Radhakrishna Naik <nikhitha.sep19@gmail.com>
2019-07-02 08:32:31 +0200

    Added Linknet and metric IoU

---
+++ code/utils/metrics.py
@@ -0,0 +1,51 @@
+import tensorflow.keras.backend as K
+
+
+SMOOTH = 1.
+
+def iou_score(gt, pr, class_weights=1., smooth=SMOOTH, per_image=True, threshold=None):
+    r""" The `Jaccard index`_, also known as Intersection over Union and the Jaccard similarity coefficient
+    (originally coined coefficient de communauté by Paul Jaccard), is a statistic used for comparing the
+    similarity and diversity of sample sets. The Jaccard coefficient measures similarity between finite sample sets,
+    and is defined as the size of the intersection divided by the size of the union of the sample sets:
+
+    .. math:: J(A, B) = \frac{A \cap B}{A \cup B}
+
+    Args:
+        gt: ground truth 4D keras tensor (B, H, W, C)
+        pr: prediction 4D keras tensor (B, H, W, C)
+        class_weights: 1. or list of class weights, len(weights) = C
+        smooth: value to avoid division by zero
+        per_image: if ``True``, metric is calculated as mean over images in batch (B),
+            else over whole batch
+        threshold: value to round predictions (use ``>`` comparison), if ``None`` prediction prediction will not be round
+
+    Returns:
+        IoU/Jaccard score in range [0, 1]
+
+
+    """
+    if per_image:
+        axes = [1, 2]
+    else:
+        axes = [0, 1, 2]
+        
+    if threshold is not None:
+        pr = K.greater(pr, threshold)
+        pr = K.cast(pr, K.floatx())
+
+    intersection = K.sum(gt * pr, axis=axes)
+    union = K.sum(gt + pr, axis=axes) - intersection
+    iou = (intersection + smooth) / (union + smooth)
+
+    # mean per image
+    if per_image:
+        iou = K.mean(iou, axis=0)
+
+    # weighted mean per class
+    iou = K.mean(iou * class_weights)
+
+    return iou
+
+
+
