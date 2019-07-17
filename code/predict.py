6529b2f0194244f02d8e2bd1645a856f27962575
Sabanaya.Mahalakshmi <Sabanaya.Mahalakshmi@lsr-ws-nv02.hpc.physik.uni-muenchen.de>
2019-07-17 19:27:38 +0200

    Few changes

--- code/predict.py
+++ code/predict.py
@@ -91,7 +91,8 @@ if __name__ == '__main__':
                               annotationpath=annotation_path,
                               is_training=False,
                               use_augmentation=False,
-                              batch_size=1).make_one_shot_iterator()
+                              batch_size=1,
+                              image_size=(shape[0], shape[1])).make_one_shot_iterator()
 
     for path in img_names:
         test_img, _ = test_set.get_next()
